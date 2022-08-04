dfdc981952c02e190ca7192cac70eee5acbbe926
JUdihouma04 <sheldonodhiuma04@gmail.com>
2022-08-04 22:58:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+const writeCards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards) {
+	let i = 0;
+	while (i < cards.lenght) {
+		console.log("Wrapped ${cards[i]}and added bowl");
+	}
+
+	return gifts;
+}
+
+writeCards;
